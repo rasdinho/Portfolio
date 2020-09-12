baed99d67ade679dab5f017a60c83b0b6d7f0e31
rasdinho <ras.dinho7@gmail.com>
2020-09-11 22:59:32 -0400

    add-contact-js-css

--- assets/js/main.js
+++ assets/js/main.js
@@ -256,3 +256,38 @@
 		}
 
 })(jQuery);
+
+
+
+var a = 0,
+    para = $('.set p').data( 'place' ),
+    paraSpan = $( '.set p span' ),
+    pArray = para.split( '' );
+
+var animate = function () {
+      
+  var timeout = setTimeout(function () {
+    clearTimeout( timeout );
+
+    if ( a === pArray.length ) {
+
+      return false;
+
+    } else {
+
+      $( '.set p' ).append( '<span>' + pArray[a] + '</span>' );
+            
+      a++;
+
+      animate();
+
+    }
+
+  }, 30 );
+};
+
+animate();
+
+    
+
+    
